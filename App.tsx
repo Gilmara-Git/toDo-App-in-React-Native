20ed24887ff506803ce0ea5be79a0d7cc168e375
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2021-02-03 21:30:24 -0500

    feat: Initiating toDo -  reactive native app

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App: React.FC = () =>{
+  return ( 
+    
+    <SafeAreaView>
+      <Text style={styles.text}>Hello World</Text>
+      <Text style={styles.text}>This is my first React Native experience.
+     </Text>
+     <Text style={styles.text}>Thanks to my friend "Ilda Neta"
+     por </Text>
+    </SafeAreaView>
+  
+  );
+};
+
+const styles = StyleSheet.create({
+
+  text: {
+ 
+    fontSize:30,
+    marginVertical:10,
+    marginHorizontal:10,
+    fontFamily: 'Montserrat-Regular',
+        
+  },
+
+});
+
+export default App;
